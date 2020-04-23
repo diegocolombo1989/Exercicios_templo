349ed8a87892c074b39ee4eb938a08b27cfa420b
DiegoColombo <diego.colombo89@gmail.com>
2020-04-23 11:51:43 -0300

    Criando lógica

--- Maior soma de pares na matriz/start.py
+++ Maior soma de pares na matriz/start.py
@@ -1,19 +1,42 @@
-lista = []
-for i in range(0,3):
-    numeros = int(input('Digite numeros: '))
-    lista.append(numeros)
-print(lista)
+# lista = []
+# numeros_lista_1 = 1
 
-lista_2 = []
-for i in range(0,3):
-    numeros = int(input('Digite numeros: '))
-    lista_2.append(numeros)
-print(lista_2)
+# while numeros_lista_1 > 0:
+#     n = int(input('Digite numeros da primeira lista e digite 0 para terminar: '))
+#     lista.append(n)
+#     if n == 0:
+#         print(lista)
+#         break
 
-def soma(lista,lista_2): return max(lista) + max(lista_2)
+
+# lista_2 = []
+# numeros_lista_2 = 1
+
+# while numeros_lista_2 > 0:
+#     n = int(input('Digite numeros da segunda lista e digite 0 para terminar: '))
+#     lista_2.append(n)
+#     if n == 0:
+#         print(lista)
+#         break
+
+# def soma(lista,lista_2):
     
 
-soma(lista,lista_2)
+# soma(lista,lista_2)
 
 
+lista = []
+for i in range(0,4):
+    i = lista.append(int(input('Digite 4 numeros: ')))
+print(max(lista))
 
+lista_2 = []
+for j in range(0,4):
+    j = lista_2.append(int(input('Digite 4 numeros: ')))
+print(max(lista_2))
+
+def soma_listas(lista,lista_2): 
+    soma = lista + lista_2
+    print(soma)
+
+soma_listas(f"{max(lista), max(lista_2)}")
