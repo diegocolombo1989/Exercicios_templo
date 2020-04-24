929980753848df38c56770ee46dc825df1b511a1
DiegoColombo <diego.colombo89@gmail.com>
2020-04-24 12:01:18 -0300

    Adicionado listas, criado função e finalizado

--- Maior soma de pares na matriz/start.py
+++ Maior soma de pares na matriz/start.py
@@ -1,42 +1,24 @@
-# lista = []
-# numeros_lista_1 = 1
+cab = '::'*100
+print(cab,'\n')
+print('\t'*9,'- MAIOR SOMA DE PARES NA MATRIZ - \n')
+print(cab,'\n')
+cab2 = '--'*50
 
-# while numeros_lista_1 > 0:
-#     n = int(input('Digite numeros da primeira lista e digite 0 para terminar: '))
-#     lista.append(n)
-#     if n == 0:
-#         print(lista)
-#         break
+primeira_lista = []
+for i in range(0,5):
+    i = primeira_lista.append(int(input('\tDigite os números: \t')))
+print(f"\n O maior número da lista digitada é: {max(primeira_lista)}",'\n',cab2,'\n')
 
-
-# lista_2 = []
-# numeros_lista_2 = 1
-
-# while numeros_lista_2 > 0:
-#     n = int(input('Digite numeros da segunda lista e digite 0 para terminar: '))
-#     lista_2.append(n)
-#     if n == 0:
-#         print(lista)
-#         break
-
-# def soma(lista,lista_2):
-    
-
-# soma(lista,lista_2)
-
-
-lista = []
-for i in range(0,4):
-    i = lista.append(int(input('Digite 4 numeros: ')))
-print(max(lista))
-
-lista_2 = []
-for j in range(0,4):
-    j = lista_2.append(int(input('Digite 4 numeros: ')))
-print(max(lista_2))
+segunda_lista = []
+for j in range(0,5):
+    j = segunda_lista.append(int(input('\tDigite os números: \t')))
+print(f"\n O maior número da lista digitada é: {max(segunda_lista)}",'\n',cab2,'\n')
 
 def soma_listas(lista,lista_2): 
-    soma = lista + lista_2
-    print(soma)
+    soma = max(primeira_lista) + max(segunda_lista)
+    print(f" O resultado da soma entre os maiores números é: {soma}\n")
+    print(cab2)
 
-soma_listas(f"{max(lista), max(lista_2)}")
+
+soma_listas(primeira_lista, segunda_lista)
+print('\t'*9,'             - FIM - ')
